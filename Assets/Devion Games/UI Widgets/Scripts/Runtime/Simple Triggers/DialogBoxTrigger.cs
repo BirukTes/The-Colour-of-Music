0f5860c5f956171fa3ece742a495df000baa6d54
Bereketab Gulai <biruktes@outlook.com>
2021-03-23 02:20:25 +0000

    UI dialog

---
+++ Assets/Devion Games/UI Widgets/Scripts/Runtime/Simple Triggers/DialogBoxTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DevionGames.UIWidgets;
+using UnityEngine.Events;
+using System;
+
+public class DialogBoxTrigger : MonoBehaviour
+{
+    public string title;
+    [TextArea]
+    public string text;
+    public Sprite icon;
+    public string[] options;
+
+    private DialogBox m_DialogBox;
+
+    private void Start()
+    {
+        this.m_DialogBox = FindObjectOfType<DialogBox>();   
+    }
+
+    public void Show() {
+        if (m_DialogBox == null)
+            Start();
+
+        m_DialogBox.Show(title, text, icon, null, options);
+    }
+
+    public void ShowWithCallback(UnityAction<int> result)
+    {
+        if (m_DialogBox == null)
+            Start();
+        m_DialogBox.Show(title, text, icon, result, options);
+    }
+
+    private void OnDialogResult(int index)
+    {
+        m_DialogBox.Show("Result", "Callback Result: "+options[index], icon, null, "OK");
+    }
+}
